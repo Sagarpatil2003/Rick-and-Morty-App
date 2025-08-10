8d76313487c9ac15d0c2018416495a356a687895
Sagarpatil2003 <sp4765699@gmal.com>
2025-08-10 17:43:12 +0530

    done the project

--- Rick-and-Morty-App/src/App.jsx
+++ Rick-and-Morty-App/src/App.jsx
@@ -1,34 +1,70 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([])
+  const [error, setError] = useState("")
+  const [page , setPage] = useState(1)
+  const [bgColor, setBgColor] = useState("lightblue")
+  //  const currentPageRef = useRef(1)
+  let postPerPage=5
+  
+  function chengeBg(){
+    setBgColor(bgColor ==="lightblue" ?"lightgreen" :"lightblue")
+  }
 
+  async function getData(){
+       try{
+          let res =  await fetch(`https://rickandmortyapi.com/api/character?page=${page}`)
+          let data = await res.json()
+          setData(data.results)
+          setError("")
+       }catch(err){
+         setError("ERROR:", err.message)
+       }
+  }
+ 
+  useEffect(()=>{
+    getData()
+  },[page])
+ 
+  let lastPage = page * postPerPage 
+  let firstPage =  lastPage - postPerPage
+  let post = data.slice(firstPage,lastPage)
+
+  if(data==null || data.length <= 0) {
+    return <h1>Loding ...</h1>
+  }
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+     
+    <div style={{ padding: '20px' }} onClick={chengeBg}>
+      <h2>Page: {page}</h2>
+      <div style={{ display: 'grid', gridTemplateColumns: 'repeat(5, 1fr)', gap: '10px' }}>
+        {post.map((char) => (
+          <div
+            key={char.id}
+            style={{
+              background: '#eee',
+              padding: '10px',
+              borderRadius: '8px',
+              color:"black",
+              textAlign: 'center'
+            }}
+          >
+            <img src={char.image} alt={char.name} style={{ width: '100%', borderRadius: '8px' }} />
+            <h4>{char.name}</h4>
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      {error && <p>{error}</p>}
+
+       <button onClick={()=> setPage(page => page-1)} disabled={page===1}> prev</button>
+       <button onClick={()=> setPage(page => page+1)}>next</button>
+       
+    </div>
   )
 }
 
